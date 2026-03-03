b0195956d9491b5b65906a6808fdd56bc923fe49
yashvithaT <148280511+yashvithaT@users.noreply.github.com>
2026-03-02 21:37:08 -0800

    Modify Runner to prompt for player names
    Updated Runner class to accept player names as input.

--- Runner.java
+++ Runner.java
@@ -3,13 +3,19 @@
  *
  * The runner for the PhraseSolverGame
  */
-public class Runner
-{
-  public static void main(String[] args) 
-  {
-    PhraseSolver p = new PhraseSolver(); 
-    p.play();
-  }
+import java.util.Scanner;
 
-} 
+public class Runner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter Player 1 name: ");
+        String name1 = input.nextLine();
+
+        System.out.print("Enter Player 2 name: ");
+        String name2 = input.nextLine();
+
+        PhraseSolver p = new PhraseSolver(name1, name2);
+        p.play();
+    }
+}
